3149d12e523512633ffc676a80cc96c77de9e41a
yanzhan91 <yanzhan91@gmail.com>
2017-05-17 00:31:39 -0500

    Fix cards pronounciation

--- scripts/my_cards_intent.py
+++ scripts/my_cards_intent.py
@@ -23,7 +23,7 @@ def get_api_response(user_id):
 
 def generate_alexa_response(status_code, cards, intent_name):
     if status_code == 200 and len(cards) != 0:
-        speech = "<speak>You have " + str(len(cards)) + (' cards' if len(cards) > 0 else ' card') \
+        speech = "<speak>You have " + str(len(cards)) + (' cards' if len(cards) > 1 else ' card') \
                  + " added.<break time='2s' />"
         for card in cards:
             speech += card + "<break time='1s' />"
