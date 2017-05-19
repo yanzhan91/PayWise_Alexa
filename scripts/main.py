baaef1e101ca44778c5580b0c783464ed381ecb3
yanzhan91 <yanzhan91@gmail.com>
2017-05-18 22:13:20 -0500

    Add required built-in intents

--- scripts/main.py
+++ scripts/main.py
@@ -12,6 +12,8 @@ def handler(event, context):
 
 
 def start_request(event):
+    if 'intent' not in event['request']:
+        return handle_empty_response()
     intent_name = event['request']['intent']['name']
     if intent_name == 'GetCardsIntent':
         return get_cards_intent.on_intent(event, intent_name)
@@ -21,5 +23,25 @@ def start_request(event):
         return set_delete_card_intent.on_intent(event, intent_name, 'delete')
     elif intent_name == 'MyCardsIntent':
         return my_cards_intent.on_intent(event, intent_name)
+    elif intent_name == 'AMAZON.CancelIntent' or intent_name == 'AMAZON.StopIntent':
+        return handle_empty_response()
+    elif intent_name == 'AMAZON.HelpIntent':
+        return handle_help_response()
     else:
         return alexa_response.generate_internal_error_response()
+
+
+def handle_empty_response():
+    return {
+        'version': '1.0',
+        'response': {
+            'shouldEndSession': True
+        }
+    }
+
+
+def handle_help_response():
+    speech = 'Welcome to Pay Wise. A service to help you maximize your credit card rewards. ' \
+             'To start, add your credit cards by simply saying add, followed by the card name. For example, add ' \
+             'Chase Freedom. Then you can ask me the name of the store such as Amazon.'
+    return alexa_response.generate_alexa_response(speech, 'PlainText', 'PayWise Help', speech, end_session=False)
